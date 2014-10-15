7356cdee19b9687d36c6221d8810d86c9c1133be
metaphorce <metaphorcedev@gmail.com>
2014-10-15 16:45:31 -0500

    Update AssemblyInfo.cs

--- Skin Changer/Skin Changer/Properties/AssemblyInfo.cs
+++ Skin Changer/Skin Changer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Skin Changer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © metaphorce 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
